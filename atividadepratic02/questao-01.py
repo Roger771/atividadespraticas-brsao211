8b26cdba3ad632163060cd378bf703451aa5d223
Roger771 <araujorg72@gmail.com>
2026-01-09 21:32:41 -0300

    Ativiade-questão-01

---
+++ atividadepratic02/questao-01.py
@@ -0,0 +1,13 @@
+valor_reais = 100.00
+
+cotacao_dolar = 5.37
+
+cotacao_euro = 6.25
+
+valor_dolar = valor_reais / cotacao_dolar
+
+valor_euro = valor_reais / cotacao_euro
+
+print(f"Valor em reias: R$ {valor_reais:.2f}")
+print(f"Valor em dolar: U$ {valor_dolar:.2f}")
+print(f"Valor em euro: £ {valor_euro:.2f}")
