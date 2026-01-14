5c1c0e2ebe190b74f58b7b24ae532b6b47fcba9d
Roger771 <araujorg72@gmail.com>
2026-01-14 20:03:13 -0300

    atividade-pratica-questao1

---
+++ atividadepratic04/questao-01.py
@@ -0,0 +1,34 @@
+while True:
+    try:
+        numero1 = float(input("Digite o primeiro número:"))
+        numero2 = float(input("Digite o segundo número:"))
+        operacao = input("Digite a operação (+, -,*, /):")
+
+        if operacao == "+":
+            resultado = numero1 + numero2
+        elif operacao == "-":
+            resultado = numero1 - numero2
+        elif operacao == "*":
+            resultado = numero1 * numero2
+        elif operacao == "/":
+            resultado = numero1 - numero2
+        else:
+            raise Exception()
+        
+        print(f"o resultado é {resultado}")
+        break
+
+    except ValueError:
+        print("Você deve digitar apenas numero")
+    except ZeroDivisionError:
+        print("Não é possivel dividir por zero")
+    except Exception:
+        print("Operação inválida")
+        
+
+
+
+
+
+
+
